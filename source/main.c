58214010cca65adf337bc17f078d2a1b1f355195
Yihua Hao <yhao024@wch136-24.cs.ucr.edu>
2020-10-14 02:02:19 -0700

    can't test

--- source/main.c
+++ source/main.c
@@ -18,11 +18,20 @@ int main(void) {
         DDRA = 0x00;
         PORTA = 0xFF;
 	PORTB = 0x00;
-        unsigned char temp_input = 0x00;
+        unsigned char tmpB = 0x00;
+	unsigned char tmpA = 0x00;
     /* Insert your solution below */
     while (1) {
-	temp_input = PINA;
-         PORTB = temp_input;
-    }
-    return 1;
-}
+// 1) Read input
+		tmpA = PINA & 0x01;
+ 		// 2) Perform computation
+ 		//if PA0 is 1, set PB1PB0 = 01, else = 10
+ 		if (tmpA == 0x01) { // True if PA0 is 1
+			tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
+						// (clear rightmost 2 bits, then set to 01)
+		 } else {												 					tmpB = (tmpB & 0xFC) | 0x02; // Sets tmpB to bbbbbb10					 												 // (clear rightmost 2 bits, then set to 10)
+	 	}		 												 			// 3) Write output
+		PORTB = tmpB;	
+		}
+		return 0;
+	}
